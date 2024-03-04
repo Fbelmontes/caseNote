311abe91f2202f42a9b6dcb963046a666779adc9
Fbelmontes <90696652+Fbelmontes@users.noreply.github.com>
2024-03-04 14:00:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Adicione mais lógica conforme necessário
 document.addEventListener('click',function(search){
-    if(search.target.closest(global)){
+    if(search.target.closest('global')){
       const start = document.querySelector('[aria-label="Create a write card"]');
       const createCard = document.querySelector('[aria-label="Create new case note"]');
       const mouseOverEvent = new MouseEvent('mouseover', {
